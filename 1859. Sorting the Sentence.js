a625108ad62e0fc6c4e9adfb49b7c5a015d9d07d
Zhamangulova Adinai <zhamangulovaadinai@Zhamangulovas-MacBook-Air.local>
2023-06-04 22:31:02 +0600

    Sorting the Sentence

---
+++ 1859. Sorting the Sentence.js
@@ -0,0 +1,12 @@
+var sortSentence = function(str) {
+    let obj={}
+let len = str.split(' ')
+let regex = /\d/g
+let matched =  str.match(regex)
+for(let i =0;i<len.length;i++){
+  obj[matched[i]] = len[i]
+}
+ return Object.keys(obj).map((e)=>obj[e]).map((word)=>{
+     return word.slice(0, word.length -1)
+ }).join(' ')
+};
